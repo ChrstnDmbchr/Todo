442b307e5f876915e41129714c4ce328d8a61843
ChrstnDmbchr <christiandambacher@gmail.com>
2018-05-03 14:50:20 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="app">
+    <h1>ToDo App</h1>
+    <div v-if="!name">
+      <h3>Hey! Please enter your name</h3>
+      <form v-on:submit="submitName">
+        <input type="text" v-model="newName" placeholder="Enter name">
+        <input type="submit" value="Submit">
+      </form>
+    </div>
+    <div v-else>
+      <h3>Hey {{name}}</h3>
+    </div>
+    <tasks></tasks>
+  </div>
+</template>
+
+<script>
+import Tasks from './components/Tasks'
+
+export default {
+  name: 'App',
+  components: {
+    Tasks
+  },
+  data () {
+    return {
+      name: '',
+      newName: ''
+    }
+  },
+  methods: {
+    submitName: function(e) {
+      this.name = this.newName
+      e.preventDefault();
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Tasks.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="tasks">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'tasks'
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
